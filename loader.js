cb5fc28f33e6138d91c43ccf7a9fe78fc330878b
forveined <120341546+forveined@users.noreply.github.com>
2023-03-28 13:40:17 -0400

    Update loader.js

--- loader.js
+++ loader.js
@@ -5,36 +5,24 @@ button.style.bottom = '20px';
 button.style.right = '20px';
 button.style.zIndex = '9999';
 
-const iframe1 = document.createElement('iframe');
-iframe1.src = 'https://chat.openai.com/';
-iframe1.width = '600';
-iframe1.height = '400';
-iframe1.style.position = 'fixed';
-iframe1.style.bottom = '20px';
-iframe1.style.right = '20px';
-iframe1.style.border = 'none';
-iframe1.style.display = 'none';
-
-const iframe2 = document.createElement('iframe');
-iframe2.src = 'https://www.google.com/';
-iframe2.width = '600';
-iframe2.height = '400';
-iframe2.style.position = 'fixed';
-iframe2.style.bottom = '20px';
-iframe2.style.right = '20px';
-iframe2.style.border = 'none';
-iframe2.style.display = 'none';
+const iframe = document.createElement('iframe');
+iframe.width = '600';
+iframe.height = '400';
+iframe.style.position = 'fixed';
+iframe.style.bottom = '20px';
+iframe.style.right = '20px';
+iframe.style.border = 'none';
+iframe.style.display = 'none';
 
 document.body.appendChild(button);
-document.body.appendChild(iframe1);
-document.body.appendChild(iframe2);
+document.body.appendChild(iframe);
 
 button.addEventListener('click', () => {
-  if (iframe1.style.display === 'none') {
-    iframe1.style.display = 'block';
-    iframe2.style.display = 'none';
+  if (iframe.style.display === 'none') {
+    iframe.style.display = 'block';
+    const url = prompt('Enter the URL you want to load:');
+    iframe.src = url;
   } else {
-    iframe1.style.display = 'none';
-    iframe2.style.display = 'block';
+    iframe.style.display = 'none';
   }
 });
